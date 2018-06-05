a24109f67a67bc70bb66e57d1671088af12274d0
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-05 11:39:40 +0900

    ~7.7

--- kaigan_js/chapter_7.js
+++ kaigan_js/chapter_7.js
@@ -7,3 +7,16 @@ var parentFunction = function() {
 
 var nestedFunction = parentFunction();
 nestedFunction();
+
+
+var logElementFunction = function () {
+  funcArray = [];
+  var i;
+  for (i = 0; i < 3; i++) {
+    funcArray[i] = function () { console.log(i); };
+  }
+
+  return funcArray;
+
+}();
+
