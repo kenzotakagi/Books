3b67bf98641d9e42947e70eee19fe69b4fc1dd8a
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-07 14:24:21 +0900

    +7.7

--- kaigan_js/chapter_7.js
+++ kaigan_js/chapter_7.js
@@ -8,15 +8,45 @@ var parentFunction = function() {
 var nestedFunction = parentFunction();
 nestedFunction();
 
+var sayWord = function() { console.log('word'); }();
 
-var logElementFunction = function () {
-  funcArray = [];
+
+var countUpFromZero = function() {
+  var count = 0;
+  return function() {
+    return ++count;
+  };
+}();
+console.log(countUpFromZero());
+console.log(countUpFromZero());
+console.log(countUpFromZero());
+
+var logElementNumber = function() {
+  var funcArray = [];
   var i;
   for (i = 0; i < 3; i++) {
-    funcArray[i] = function () { console.log(i); };
+    funcArray[i] = function() { console.log(i); };
   }
-
   return funcArray;
-
 }();
+logElementNumber[0]();
+logElementNumber[1]();
+logElementNumber[2]();
+
+
+var logElementNumber = function() {
+  var funcArray = [];
+  var i;
+  var func = function(i) {
+    return function() { console.log(i); };
+  };
+  for (i = 0; i < 3; i++) {
+    funcArray[i] = func(i);
+  }
+  return funcArray;
+}();
+logElementNumber[0]();
+logElementNumber[1]();
+logElementNumber[2]();
+
 
