0203cbd578673ac70f7ca6bd18b90ccd6dac0c3b
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-08 23:40:20 +0900

    8.8

--- kaigan_js/chapter_8.js
+++ kaigan_js/chapter_8.js
@@ -5,3 +5,13 @@ console.log(myFunction.prototype);
 Array.prototype.foo = 'foo';
 var myAry = new Array();
 console.log(myAry.__proto__.foo);
+
+var myFunction = function() {};
+console.log(myFunction.prototype);
+console.log(typeof myFunction.prototype);
+
+var Foo = function Foo() {};
+Foo.prototype = {};
+var fooInstance = new Foo();
+console.log(fooInstance.constructor === Foo);
+
