d96a02e438c9ed45fab705e2305c5811455ef2de
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-11 18:06:53 +0900

    27

--- WEB+DB_PRESS_vol87/chapter_3.js
+++ WEB+DB_PRESS_vol87/chapter_3.js
@@ -67,3 +67,23 @@ console.log(obj.foo, obj.bar);
 //es6
 var obj = { foo, bar }
 console.log(obj.foo, obj.bar);
+
+//es6
+class Author extends Person {
+  constructor(name, book) {
+    super(name);
+    this.book = book;
+  }
+
+  greet() {
+    super.greet();
+    console.log("I wrote " + this.book);
+  }
+
+  static create(name, book) {
+    return new Author(name, book);
+  }
+}
+
+var author = new Author("kenzo", "my first kiss");
+author.greet();
