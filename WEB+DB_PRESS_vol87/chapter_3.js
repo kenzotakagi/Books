91250b249d16a923500a27257a41712f92a87686
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-11 17:06:45 +0900

    25

--- WEB+DB_PRESS_vol87/chapter_3.js
+++ WEB+DB_PRESS_vol87/chapter_3.js
@@ -2,12 +2,31 @@
 var add = function(a, b) {
   return a + b;
 };
-
 //es6
 var add = (a, b) => {
   return a + b;
 };
-
 var add = (a, b) => a + b;
-
 var square = n => n * n;
+
+//es5
+var john = {
+  name: "john",
+  helloLater: function() {
+    var self = this;
+    setTimeout(function() {
+      console.log("Hello I'm " + self.name);
+    }, 1000);
+  }
+}
+john.helloLater();
+
+//es6
+var john = {
+  name: "john",
+  helloLater: function() {
+    setTimeout(() => {
+      console.log("Hello I'm " + this.name);
+    }, 1000);
+  }
+}
