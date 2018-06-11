5291c2dd161de3108c1bc46b7b80451b082b8356
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-11 16:53:08 +0900

    25

--- WEB+DB_PRESS_vol87/chapter_1.js
+++ WEB+DB_PRESS_vol87/chapter_1.js
@@ -14,3 +14,5 @@ class counter {
 }
 
 export default Counter;
+
+
---
+++ WEB+DB_PRESS_vol87/chapter_3.js
@@ -0,0 +1,13 @@
+//es5
+var add = function(a, b) {
+  return a + b;
+};
+
+//es6
+var add = (a, b) => {
+  return a + b;
+};
+
+var add = (a, b) => a + b;
+
+var square = n => n * n;
