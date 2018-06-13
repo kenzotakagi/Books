2eb9f74ccc3f886d2591de46d4c097504d763bfd
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-13 17:20:12 +0900

    refer

--- WEB+DB_PRESS_vol87/chapter_4.js
+++ WEB+DB_PRESS_vol87/chapter_4.js
@@ -1,3 +1,23 @@
-var map = new Map();
-map.set("key", "value1");
-map.get("key");
+let data_ary = ["one", "two", "three"];
+let data_str = "あいうえお"
+let data_map = new Map([["Mon", "月曜"], ["Tue", "火曜"], ["Wed", "水曜"]])
+
+for(let d of data_ary) {
+  console.log(d)
+}
+
+for(let d of data_str) {
+  console.log(d)
+}
+
+for(let [key, value] of data_map) {
+  console.log(`${key} : ${value}`);
+}
+
+let itr = data_ary[Symbol.iterator]();
+let d;
+while(d = itr.next()) {
+  if (d.done) { break; }
+  console.log(d.done);
+  console.log(d.value);
+}
