3d432197ef3498b485bf3aac35b2be0582f7dcbb
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-20 02:39:36 +0900

    5.1.5

--- honkaku_js/chapter_5.js
+++ honkaku_js/chapter_5.js
@@ -11,3 +11,13 @@ var Member = function(firstName, lastName){
   }
 }
 var mem = new Member('kenzo', 'takagi');
+
+var data = 'Global data';
+var obj1 = { data: 'obj1 data' };
+var obj2 = { data: 'obj2 data' };
+function hoge() {
+  console.log(this.data);
+}
+hoge.call(null);
+hoge.call(obj1);
+hoge.call(obj2);
