470a94eb0f800a3f439083025f83ac9552397b5e
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-21 14:39:24 +0900

    163

--- practical-object-oriented-design-in-ruby/chapter_6.rb
+++ practical-object-oriented-design-in-ruby/chapter_6.rb
@@ -1,8 +1,14 @@
 class Bycicle
-  attr_reader :size
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args = {})
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end
+
+  def default_chain
+    '10-speed'
   end
 end
 
@@ -13,7 +19,18 @@ class RoadBike < Bycicle
     @tape_color = args[:tape_color]
     super(args)
   end
+
+  def default_tire_size
+    '23'
+  end
 end
 
 class MountainBike < Bycicle
+  def default_tire_size
+    '2.1'
+  end
 end
+
+road_bike = RoadBike.new(size: 'M', tape_color: 'red')
+puts road_bike.tire_size
+puts road_bike.chain
