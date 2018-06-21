ef982866a24ad1fd0aa5dc3bc39ffdb115c427ef
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-21 14:13:23 +0900

    158

--- practical-object-oriented-design-in-ruby/chapter_6.rb
+++ practical-object-oriented-design-in-ruby/chapter_6.rb
@@ -1,7 +1,18 @@
 class Bycicle
+  attr_reader :size
+
+  def initialize(args = {})
+    @size = args[:size]
+  end
 end
 
 class RoadBike < Bycicle
+  attr_reader :tape_color
+
+  def initialize(args)
+    @tape_color = args[:tape_color]
+    super(args)
+  end
 end
 
 class MountainBike < Bycicle
