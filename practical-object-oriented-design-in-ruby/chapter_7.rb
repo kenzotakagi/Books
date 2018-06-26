3947a0ca7738f748360d9fc65071503c12c61bf0
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-06-27 00:09:51 +0900

    191

--- practical-object-oriented-design-in-ruby/chapter_7.rb
+++ practical-object-oriented-design-in-ruby/chapter_7.rb
@@ -4,12 +4,40 @@ class Schedule
     false
   end
 end
+#
+# class Bicycle
+#   attr_reader :schedule, :size, :chain, :tire_size
+#
+#   def initialize(args = {})
+#     @schedule = args[:schedule] || Schedule.new
+#   end
+#
+#   def schedulable?(start_date, end_date)
+#     puts !scheduled?(start_date - lead_days, end_date)
+#   end
+#
+#   def scheduled?(start_date, end_date)
+#     schedule.scheduled?(self, start_date, end_date)
+#   end
+#
+#   def lead_days
+#     1
+#   end
+# end
+#
+# require 'date'
+# starting = Date.parse('2018/06/26')
+# ending = Date.parse('2018/06/30')
+# b = Bicycle.new
+# b.schedulable?(starting, ending)
 
-class Bicycle
-  attr_reader :schedule, :size, :chain, :tire_size
+##################
 
-  def initialize(args = {})
-    @schedule = args[:schedule] || Schedule.new
+module Schedulable
+  attr_writer :schedule
+
+  def schedule
+    @schedule || ::Schedule.new
   end
 
   def schedulable?(start_date, end_date)
@@ -21,6 +49,14 @@ class Bicycle
   end
 
   def lead_days
+    0
+  end
+end
+
+class Bicycle
+  include Schedulable
+
+  def lead_days
     1
   end
 end
@@ -30,23 +66,3 @@ starting = Date.parse('2018/06/26')
 ending = Date.parse('2018/06/30')
 b = Bicycle.new
 b.schedulable?(starting, ending)
-
-#module Schedulable
-#  attr_writer :schedule
-#
-#  def schedule
-#    @schedule || ::Schedule.new
-#  end
-#
-#  def schedulable?(start_date, end_date)
-#    !scheduled?(start_date - lead_days, end_date)
-#  end
-#
-#  def scheduled?(start_date, end_date)
-#    schedule.scheduled?(self, start_date, end_date)
-#  end
-#
-#  def lead_days
-#    0
-#  end
-#end
