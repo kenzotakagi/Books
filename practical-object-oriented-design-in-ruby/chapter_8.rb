6a1ead79e995e63233a42099436d84c44cefcb8b
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-07-03 02:47:40 +0900

    219

--- practical-object-oriented-design-in-ruby/chapter_8.rb
+++ practical-object-oriented-design-in-ruby/chapter_8.rb
@@ -1,4 +1,4 @@
-class Bycicle
+class Bicycle
   attr_reader :size, :parts
 
   def initialize(args = {})
@@ -36,72 +36,15 @@ class Part
   end
 end
 
-##################
+chain =         Part.new(name: 'chain',      description: '10-speed')
+road_tire =     Part.new(name: 'tire_size',  description: '23')
+tape =          Part.new(name: 'tape_color', description: 'red')
+mountain_tire = Part.new(name: 'tire_size',  description: '2.1')
+rear_shock =    Part.new(name: 'rear_shock', description: 'Fox')
+front_shock =   Part.new(name: 'front_shock',
+                         description: 'Manitou',
+                         needs_spare: false)
 
-class Parts
-  attr_reader :chain, :tire_size
-
-  def initialize(args = {})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    post_initialize(args)
-  end
-
-  def post_initialize(args)
-    nil
-  end
-
-  def local_spares
-    {}
-  end
-
-  def spares
-    { tire_size: tire_size,
-      chain: chain }.merge(local_spares)
-  end
-
-  def default_chain
-    '10-speed'
-  end
-end
-
-class RoadBikeParts < Parts
-  attr_reader :tape_color
-
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-
-  def default_tire_size
-    '23'
-  end
-end
-
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
-
-  def post_initialize(args)
-    @front_shock = args[:ront_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def local_spares
-    { rear_shock: rear_shock }
-  end
-
-  def default_tire_size
-    '2.1'
-  end
-end
-
-road_bike = Bycicle.new(size: 'L', parts: RoadBikeParts.new(tape_color: 'red'))
-puts road_bike.size
-puts road_bike.spares
-mountain_bike = Bycicle.new(size: 'L', parts: MountainBikeParts.new(rear_shock: 'Fox'))
-puts mountain_bike.size
-puts mountain_bike.spares
-
+mountain_bike = Bicycle.new(size: 'L', parts: Parts.new([chain, front_shock, rear_shock]))
+puts mountain_bike.spares.size
+puts mountain_bike.parts.size
