731422b7d05e7a8bc7ff8fa129d6c82fb61144f7
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-07-03 02:10:41 +0900

    213

--- practical-object-oriented-design-in-ruby/chapter_8.rb
+++ practical-object-oriented-design-in-ruby/chapter_8.rb
@@ -12,6 +12,30 @@ class Bycicle
 end
 
 class Parts
+  attr_reader :parts
+
+  def initialize(parts)
+    @parts = parts
+  end
+
+  def spares
+    parts.select { |part| part.needs_spare}
+  end
+end
+
+##################
+
+class Part
+  attr_reader :name, :description, :needs_spare
+
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
+  end
+end
+
+class Parts
   attr_reader :chain, :tire_size
 
   def initialize(args = {})
