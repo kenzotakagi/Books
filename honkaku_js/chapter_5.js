37641da7c8cca66d57a222d5750ae0ac4f55ab89
KenzaburoTakagi <ken.takagi216@gmail.com>
2018-07-05 16:26:58 +0900

    honkaku_js 5-10

--- honkaku_js/chapter_5.js
+++ honkaku_js/chapter_5.js
@@ -28,3 +28,15 @@ var Member = function(firstName, lastName) {
   }
   this.firstName = firstName;
 };
+
+var Member = function(firstName, lastName){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  };
+
+Member.prototype.getName = function() {
+  return this.lastName + '' + this.firstName;
+};
+
+var mem = new Member('takagi', 'kenzo')
+console.log(mem.getName());
