cb0c31597d327277fa787616315d14c7c80fa90b
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-03-27 16:48:59 +0900

    13-3

--- sukkiri-java/rpg/Hero.java
+++ sukkiri-java/rpg/Hero.java
@@ -1,30 +1,10 @@
 public class Hero {
-  String name;
-  int hp;
-  void sleep() {
-    this.hp = 100;
-    System.out.println(this.name + "は眠って回復した!");
-  }
-  Hero(String name) {
-    this.hp = 100;
-    this.name = name;
-  }
-  Hero() {
-    this("ダミー");
-  }
-  void sit(int sec) {
-    this.hp += sec;
-    System.out.println(this.name + "は、" + sec + "秒座った!");
-    System.out.println("HPが" + sec + "ポイント回復した！");
-  }
-  void slip() {
-    this.hp -= 5;
-    System.out.println(this.name + "は、転んだ!");
-    System.out.println("5のダメージ!");
-  }
-  void run() {
-    System.out.println(this.name + "は、逃げ出した！");
-    System.out.println("GameOver");
-    System.out.println("最終HPは" + this.hp + "でした");
+  public static void main (String[] args) {
+    Y[] array = new Y[2];
+    array[0] = new A();
+    array[1] = new B();
+    for (Y y : array) {
+      y.b();
+    }
   }
 }
