5968f8b7b77c0cdd3ec657d27f556ee8336aad38
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-04-03 21:53:50 +0900

    12-1

---
+++ sukkiri-java/12/TangibleAsset.java
@@ -0,0 +1,33 @@
+public abstract class TangibleAsset extends Asset {
+  private String name;
+  private int price;
+  private String color;
+  public TangibleAsset(String name, int price, String color) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
+  public String getName() { return this.name; }
+  public int getPrice() { return this.price; }
+  public String getColor() { return this.color; }
+}
+
+public class Book extends TangibleAsset {
+  private String isbn;
+  public Book
+    (String name, int price, String color, String isbn) {
+      super(name, price, color);
+      this.isbn = isbn;
+    }
+  public String getIsbn() { return this.isbn; }
+}
+
+public class Computer extends TangibleAsset {
+  private String makerName;
+  public Computer
+    (String name, int price, String color, String makerName) {
+      super(name, price, color);
+      this.makerName = makerName;
+    }
+  public String getMakerName() { return this.makerName; }
+}
