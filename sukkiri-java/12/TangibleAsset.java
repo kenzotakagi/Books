367f09914cbac8afd8f765496e8bb8831379ca52
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-04-03 22:25:54 +0900

    12-4

--- sukkiri-java/12/TangibleAsset.java
+++ sukkiri-java/12/TangibleAsset.java
@@ -14,15 +14,16 @@ public abstract class Asset {
   public int getPrice() { return this.price; }
 }
 
-public abstract class TangibleAsset extends Asset {
-  private String name;
-  private int price;
+public abstract class TangibleAsset extends Asset implements Thing {
   private String color;
+  private double weight;
   public TangibleAsset(String name, int price, String color) {
     super(name, price);
     this.color = color;
   }
   public String getColor() { return this.color; }
+  public double getWeight() { return this.weight; }
+  public void setWeight(double weight) { this.weight = weight; }
 }
 
 public class Book extends TangibleAsset {
