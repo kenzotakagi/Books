d40e53bd3d5566df2ba93f5baf53f8448a681a05
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-04-06 23:07:44 +0900

    15-3

--- sukkiri-java/15/Main.java
+++ sukkiri-java/15/Main.java
@@ -1,13 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     try {
-      String s = null;
-      System.out.println(s.length());
-    } catch (Exception e) {
-      System.out.println("NullPointerException例外をcatchしました");
-      System.out.println("--スタックトレース(ここから)");
-      e.printStackTrace();
-      System.out.println("--スタックトレース(ここまで)");
+      int i = Integer.parseInt("三");
+    } catch (NumberFormatException e) {
+      System.out.println("例外catch");
     }
   }
 }
