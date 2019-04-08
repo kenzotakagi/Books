8d5065a397e1670ec8ef05934719f143481bc769
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-04-08 23:14:41 +0900

    2-2

--- jissen-sukkiri-java/2/Main.java
+++ jissen-sukkiri-java/2/Main.java
@@ -1,17 +1,11 @@
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
   public static void main(String[] args) {
-    Date now = new Date();
-    Calendar c = Calendar.getInstance();
-    c.setTime(now);
-    int day = c.get(Calendar.DAY_OF_MONTH);
-    day += 100;
-    c.set(Calendar.DAY_OF_MONTH, day);
-    Date future = c.getTime();
-    SimpleDateFormat f = new SimpleDateFormat("西暦yyyy年MM月dd日");
-    System.out.println(f.format(future));
+    LocalDate now = LocalDate.now();
+    LocalDate future = now.plusDays(100);
+    DateTimeFormatter f = DateTimeFormatter.ofPattern("西暦yyyy年MM月dd日");
+    System.out.println(future.format(f));
   }
 }
