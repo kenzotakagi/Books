d08976a0d967d1414dd75dc50d9a926e089c4a79
KenzaburoTakagi <ken.takagi216@gmail.com>
2019-04-18 23:05:01 +0900

    6-1

---
+++ jissen-sukkiri-java/6/Main.java
@@ -0,0 +1,17 @@
+interface Func {
+  boolean call(int x);
+}
+
+interface Func2 {
+  String call(boolean male, String name);
+}
+
+public class Main {
+  public static void main(String[] args) {
+    FuncList funcList = new FuncList();
+    Func1 f1 = FuncList::isOdd;
+    Func2 f2 = FuncList::addNamePrefix;
+    System.out.println(f1.call(15));
+    System.out.println(f2.call(true, "Smith"));
+  }
+}
